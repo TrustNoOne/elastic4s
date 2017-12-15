e4d2c06d1909b9e8f4211466dd82adfe8bd08979
Giovanni Caporaletti <g.caporaletti@reaqta.com>
2017-12-15 18:44:58 +0100

    add ElastisearchDate

---
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/ElasticsearchDate.scala
@@ -0,0 +1,116 @@
+package com.sksamuel.elastic4s
+
+import com.sksamuel.elastic4s.json.XContentBuilder
+
+sealed trait ElasticsearchTimeUnit {
+  private[elastic4s] def unit: String
+}
+
+object ElasticsearchTimeUnit {
+
+  case object Years extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "y"
+  }
+
+  case object Months extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "M"
+  }
+
+  case object Weeks extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "w"
+  }
+
+  case object Days extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "d"
+  }
+
+  case object Hours extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "h"
+  }
+
+  case object Minutes extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "m"
+  }
+
+  case object Seconds extends ElasticsearchTimeUnit {
+    private[elastic4s] def unit: String = "s"
+  }
+
+}
+
+/**
+  * An elasticsearch date. It can be a number or a string,
+  * see https://www.elastic.co/guide/en/elasticsearch/reference/current/date.html
+  */
+sealed trait ElasticsearchDate {
+  type Date
+
+  def date: Date
+
+  private[elastic4s] def addField(builder: XContentBuilder, name: String): XContentBuilder
+}
+
+object ElasticsearchDate {
+
+  val Now = ElasticsearchStringDate("now")
+
+  def fromString(date: String): ElasticsearchStringDate =
+    ElasticsearchStringDate(date)
+
+  def fromEpochMillis(date: Long): ElasticsearchEpochDate =
+    ElasticsearchEpochDate(date)
+}
+
+
+private[elastic4s] case class ElasticsearchStringDate(date: String) extends ElasticsearchDate {
+  type Date = String
+
+  /**
+    * Elasticsearch Date Math
+    * see https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#date-math
+    */
+  def plus(amount: Int, unit: ElasticsearchTimeUnit): ElasticsearchStringDate =
+    dateMath("+", amount.toString, unit.unit)
+
+  def minus(amount: Int, unit: ElasticsearchTimeUnit): ElasticsearchStringDate =
+    dateMath("-", amount.toString, unit.unit)
+
+  def roundedTo(unit: ElasticsearchTimeUnit): ElasticsearchStringDate =
+    dateMath("/", "", unit.unit)
+
+  def hasDateMath: Boolean = date.startsWith(ElasticsearchDate.Now.date) || date.contains("||")
+
+  private def dateMath(operation: String,
+                       amount: String,
+                       unit: String
+                      ): ElasticsearchStringDate = {
+    if (hasDateMath) ElasticsearchStringDate(s"$date$operation$amount$unit")
+    else ElasticsearchStringDate(s"$date||$operation$amount$unit")
+  }
+
+  def plusYears(years: Int): ElasticsearchStringDate = plus(years, ElasticsearchTimeUnit.Years)
+  def plusMonths(months: Int): ElasticsearchStringDate = plus(months, ElasticsearchTimeUnit.Months)
+  def plusWeeks(weeks: Int): ElasticsearchStringDate = plus(weeks, ElasticsearchTimeUnit.Weeks)
+  def plusDays(days: Int): ElasticsearchStringDate = plus(days, ElasticsearchTimeUnit.Days)
+  def plusHours(hours: Int): ElasticsearchStringDate = plus(hours, ElasticsearchTimeUnit.Hours)
+  def plusMinutes(minutes: Int): ElasticsearchStringDate = plus(minutes, ElasticsearchTimeUnit.Minutes)
+  def plusSeconds(seconds: Int): ElasticsearchStringDate = plus(seconds, ElasticsearchTimeUnit.Seconds)
+
+  def minusYears(years: Int): ElasticsearchStringDate = minus(years, ElasticsearchTimeUnit.Years)
+  def minusMonths(months: Int): ElasticsearchStringDate = minus(months, ElasticsearchTimeUnit.Months)
+  def minusWeeks(weeks: Int): ElasticsearchStringDate = minus(weeks, ElasticsearchTimeUnit.Weeks)
+  def minusDays(days: Int): ElasticsearchStringDate = minus(days, ElasticsearchTimeUnit.Days)
+  def minusHours(hours: Int): ElasticsearchStringDate = minus(hours, ElasticsearchTimeUnit.Hours)
+  def minusMinutes(minutes: Int): ElasticsearchStringDate = minus(minutes, ElasticsearchTimeUnit.Minutes)
+  def minusSeconds(seconds: Int): ElasticsearchStringDate = minus(seconds, ElasticsearchTimeUnit.Seconds)
+
+  override private[elastic4s] def addField(builder: XContentBuilder, name: String): XContentBuilder =
+    builder.field(name, date)
+}
+
+private[elastic4s] case class ElasticsearchEpochDate(date: Long) extends ElasticsearchDate {
+  type Date = Long
+
+  override private[elastic4s] def addField(builder: XContentBuilder, name: String): XContentBuilder =
+    builder.field(name, date)
+}
---
+++ elastic4s-tests/src/test/scala/com/sksamuel/elastic4s/ElasticsearchDateTest.scala
@@ -0,0 +1,87 @@
+package com.sksamuel.elastic4s
+
+import com.sksamuel.elastic4s.ElasticsearchTimeUnit._
+import org.scalatest.{Matchers, WordSpec}
+
+class ElasticsearchDateTest extends WordSpec with Matchers {
+
+  "ElasticsearchDate" should {
+    "parse a string date" in {
+      ElasticsearchDate.fromString("2018-01-01") shouldBe ElasticsearchStringDate("2018-01-01")
+    }
+
+    "parse an epoch date" in {
+      ElasticsearchDate.fromEpochMillis(1234567890L) shouldBe ElasticsearchEpochDate(1234567890L)
+    }
+
+    "add amounts of time" in {
+      val d1 = ElasticsearchDate.fromString("2018-01-01")
+      d1.plus(1, Years).date shouldBe "2018-01-01||+1y"
+      d1.plus(2, Months).date shouldBe "2018-01-01||+2M"
+      d1.plus(3, Weeks).date shouldBe "2018-01-01||+3w"
+      d1.plus(4, Days).date shouldBe "2018-01-01||+4d"
+      d1.plus(5, Hours).date shouldBe "2018-01-01||+5h"
+      d1.plus(6, Minutes).date shouldBe "2018-01-01||+6m"
+      d1.plus(7, Seconds).date shouldBe "2018-01-01||+7s"
+    }
+
+    "add amounts of time (helper methods)" in {
+      val d1 = ElasticsearchDate.fromString("2018-01-01")
+      d1.plusYears(1) shouldBe d1.plus(1, Years)
+      d1.plusMonths(2) shouldBe d1.plus(2, Months)
+      d1.plusWeeks(3) shouldBe d1.plus(3, Weeks)
+      d1.plusDays(4) shouldBe d1.plus(4, Days)
+      d1.plusHours(5) shouldBe d1.plus(5, Hours)
+      d1.plusMinutes(6) shouldBe d1.plus(6, Minutes)
+      d1.plusSeconds(7) shouldBe d1.plus(7, Seconds)
+    }
+
+    "subtract amounts of time" in {
+      val d1 = ElasticsearchDate.fromString("2018-01-01")
+      d1.minus(1, Years).date shouldBe "2018-01-01||-1y"
+      d1.minus(2, Months).date shouldBe "2018-01-01||-2M"
+      d1.minus(3, Weeks).date shouldBe "2018-01-01||-3w"
+      d1.minus(4, Days).date shouldBe "2018-01-01||-4d"
+      d1.minus(5, Hours).date shouldBe "2018-01-01||-5h"
+      d1.minus(6, Minutes).date shouldBe "2018-01-01||-6m"
+      d1.minus(7, Seconds).date shouldBe "2018-01-01||-7s"
+    }
+
+    "subtract amounts of time (helper methods)" in {
+      val d1 = ElasticsearchDate.fromString("2018-01-01")
+      d1.minusYears(1) shouldBe d1.minus(1, Years)
+      d1.minusMonths(2) shouldBe d1.minus(2, Months)
+      d1.minusWeeks(3) shouldBe d1.minus(3, Weeks)
+      d1.minusDays(4) shouldBe d1.minus(4, Days)
+      d1.minusHours(5) shouldBe d1.minus(5, Hours)
+      d1.minusMinutes(6) shouldBe d1.minus(6, Minutes)
+      d1.minusSeconds(7) shouldBe d1.minus(7, Seconds)
+    }
+
+    "round down to a time unit" in {
+      val d1 = ElasticsearchDate.fromString("2018-01-01")
+      d1.roundedTo(Years).date shouldBe "2018-01-01||/y"
+      d1.roundedTo(Months).date shouldBe "2018-01-01||/M"
+      d1.roundedTo(Weeks).date shouldBe "2018-01-01||/w"
+      d1.roundedTo(Days).date shouldBe "2018-01-01||/d"
+      d1.roundedTo(Hours).date shouldBe "2018-01-01||/h"
+      d1.roundedTo(Minutes).date shouldBe "2018-01-01||/m"
+      d1.roundedTo(Seconds).date shouldBe "2018-01-01||/s"
+    }
+
+    "support 'now' date" in {
+      val now = ElasticsearchDate.fromString("now")
+      now shouldBe ElasticsearchDate.Now
+      now.plus(1, Years).date shouldBe "now+1y"
+      now.minus(2, Days).date shouldBe "now-2d"
+    }
+
+    "support multiple date math operations" in {
+      ElasticsearchDate.Now.plusDays(1).minusHours(2).roundedTo(Months)
+        .date shouldBe "now+1d-2h/M"
+
+      ElasticsearchDate.fromString("2018-01-01").minusSeconds(1).roundedTo(Days).plusHours(1)
+          .date shouldBe "2018-01-01||-1s/d+1h"
+    }
+  }
+}
